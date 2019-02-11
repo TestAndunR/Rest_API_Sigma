46bf7672eb4954e1e67756cf01bcc70a206cf371
TestAndunR <andunranmal@gmail.com>
2019-02-11 11:31:35 +0530

    getUser

--- Rest_API_Sigma/getUser.js
+++ Rest_API_Sigma/getUser.js
@@ -2,25 +2,23 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
-
-    let userId = event.body.userId;
-    let userName = event.body.name;
-    ddb.put({
+    let userId = event.queryStringParameters.userId;
+    ddb.get({
         TableName: 'users-dbtable-serverless-dev',
-        Item: { 'userId': userId, 'name': userName }
+        Key: { 'userId': userId }
     }).promise().then(function (data) {
         //your logic goes here
         let response = {
             statusCode: 200,
-            body: "User added successfully"
+            body: JSON.stringify(result.Item)
         }
         callback(null, response)
         return
     }).catch(function (err) {
         //handle error
-        callback(new Error('Couldn\'t add user.'))
+        callback(new Error("Coudn't get user"), null)
         return
     });
 
-    // callback(null, { "message": "Successfully executed" });
+    callback(null, { "message": "Successfully executed" });
 }
